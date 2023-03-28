9405db4f1581176a4e2cd93accff2a5d286d4710
iAmWayneBenedict <iraolawayne@gmail.com>
2023-03-28 08:28:51 +0800

    init project

---
+++ resources/js/home.js
@@ -0,0 +1,7 @@
+import {
+    regions,
+    provinces,
+    cities,
+    barangays,
+} from "select-philippines-address";
+regions().then((region) => console.log(region));
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        <!-- Styles -->
+         @vite(['resources/css/app.css', 'resources/js/app.js'])
+         @vite(['resources/js/home.js'])
+    </head>
+    <body>
+        <script>
+
+        </script>
+    </body>
+</html>
