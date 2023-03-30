199a1d2181491f2d7b15231e3fc7abcd3cee4e80
Christian Quelonio <chrquelonio@gmail.com>
2023-03-30 14:57:25 +0800

    init admin side

---
+++ app/Http/Livewire/Admin/ExamApplicants.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use Livewire\Component;
+
+class ExamApplicants extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.exam-applicants');
+    }
+}
---
+++ app/Http/Livewire/CMS/Announcements.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Cms;
+
+use Livewire\Component;
+
+class Announcements extends Component
+{
+    public function render()
+    {
+        return view('livewire.cms.announcements');
+    }
+}
