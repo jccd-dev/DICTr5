beb13dc66354a59082c4cda83b59fc3d046442bd
Christian Quelonio <chrquelonio@gmail.com>
2023-03-30 16:15:09 +0800

    updated Calendar to CMS

--- app/Http/Livewire/CMS/EventCalendar.php
+++ app/Http/Livewire/CMS/EventCalendar.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Livewire\Admin;
+namespace App\Http\Livewire\Cms;
 
 use Livewire\Component;
 
@@ -8,6 +8,6 @@ class EventCalendar extends Component
 {
     public function render()
     {
-        return view('livewire.admin.event-calendar');
+        return view('livewire.cms.event-calendar');
     }
 }
