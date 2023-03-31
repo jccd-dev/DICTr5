4a628d197cd65ff02d84e906330f811abbb640e8
iAmWayneBenedict <iraolawayne@gmail.com>
2023-03-31 12:12:54 +0800

    remove test text

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -4,7 +4,7 @@
       <div class="flex items-center flex-grow">
         <div class="flex-shrink-0">
           <h1 class="text-lg font-bold tracking-wider text-black uppercase">
-            <a href="#">GOVPH</a>test
+            <a href="#">GOVPH</a>
             <span class="mx-3 font-semibold hidden xl:flex">|</span>
           </h1>
         </div>
