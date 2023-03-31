526154905d60830eddb47690892dba250f08c9f7
konnichiwaJCCD <jccd0724@gmail.com>
2023-03-31 12:00:48 +0800

    Fixed some issues in scripts

--- gitBash/git.pull.sh
+++ gitBash/git.pull.sh
@@ -6,9 +6,15 @@ git pull origin dict
 # Install dependencies packages using composer
 composer install
 # Notify that Composer installation is complete
+echo " "
+echo "-------------------------------------------"
 echo "Composer packages installation is complete."
+echo "-------------------------------------------"
 
 # Install Yarn packages
 yarn install
 # Notify that Composer installation is complete
+echo " "
+echo "---------------------------------------"
 echo "Yarn packages installation is complete."
+echo "---------------------------------------"
--- gitBash/git.push.sh
+++ gitBash/git.push.sh
@@ -11,16 +11,29 @@ git commit -m "$message"
 
 # Attempt to push the changes to the remote repository
 if git push -u origin dict; then
+  echo " "
+  echo "----------------------------"
   echo "Push successful."
+  echo "----------------------------"
+  echo " "
 else
   # If the push failed, perform a git pull to update the local repository
+  echo " "
+  echo "--------------------------------------------------------------------"
   echo "Push failed. Performing a git pull to update the local repository..."
+  echo "--------------------------------------------------------------------"
+  echo " "
   git pull origin dict
 
+  echo " "
   # Attempt to push the changes to the remote repository again
   if git push -u origin dict; then
+    echo "------------------------------------------"
     echo "Push successful after resolving conflicts."
+    echo "------------------------------------------"
   else
+    echo "-----------------------------------------------"
     echo "Push failed. Please resolve conflicts manually."
+    echo "-----------------------------------------------"
   fi
 fi
