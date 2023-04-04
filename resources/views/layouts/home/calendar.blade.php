ae3e735d9a410d77b8b536b773494db51eb6dbbb
iAmWayneBenedict <iraolawayne@gmail.com>
2023-04-04 08:15:55 +0800

    updated calendar ui section and fixed nav hover

--- resources/views/layouts/home/calendar.blade.php
+++ resources/views/layouts/home/calendar.blade.php
@@ -87,8 +87,8 @@
                                 <div x-show="isToday(date)" class="flex items-center justify-center w-full rounded-full cursor-pointer">
                                     <a role="link" x-text="date" tabindex="0" class="focus:outline-none  focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 focus:bg-indigo-500 hover:bg-indigo-500 text-base w-8 h-8 flex items-center justify-center font-medium text-white bg-indigo-700 rounded-full"></a>
                                 </div>
-                                <div x-show="!isToday(date)" class="px-2 py-2 cursor-pointer flex w-full justify-center">
-                                    <p class="text-base text-gray-500 dark:text-gray-100 font-medium" x-text="date"></p>
+                                <div x-show="!isToday(date)" class="flex items-center justify-center w-full rounded-full cursor-pointer">
+                                    <p class="text-base text-gray-500 dark:text-gray-100 font-medium hover:bg-slate-200 w-8 h-8 rounded-full flex justify-center items-center" x-text="date"></p>
                                 </div>
                             </div>
                         </template>
---
+++ resources/views/layouts/home/section/calendar-section.blade.php
@@ -0,0 +1,46 @@
+<div class="flex justify-center mt-20 gap-10">
+    @include('layouts.home.calendar')
+    <div class="w-[35rem] bg-yellow-300 rounded-xl p-10">
+        <h1 class="font-bold text-2xl font-quicksand">April 2023 Highlight Activities</h1>
+        <div class="legends flex gap-5 mt-2">
+            <div class="flex gap-2 items-center">
+                <div class="legend-icon rounded-full bg-[#51B800] w-3 h-3"></div>
+                <h4>Ongoing Events</h4>
+            </div>
+            <div class="flex gap-2 items-center">
+                <div class="legend-icon rounded-full bg-[#00296B] w-3 h-3"></div>
+                <h4>Upcoming Events</h4>
+            </div>
+            <div class="flex gap-2 items-center">
+                <div class="legend-icon rounded-full bg-[#C1121F] w-3 h-3"></div>
+                <h4>Past Events</h4>
+            </div>
+        </div>
+        <div class="flex flex-col mt-5 gap-3">
+            <div class="bg-white rounded-xl px-5 py-3 flex items-center gap-5">
+                <div class="legend-icon rounded-full bg-[#51B800] w-3 h-3"></div>
+                <div>
+                    <h1 class="font-bold font-quicksand text-lg mb-1">LGU Pili Tech4ED Center Launching</h1>
+                    <p class="font-quicksand text-sm">March 28, 2023</p>
+                    <p class="mt-0 font-quicksand text-sm">08:00AM - 5:00AM</p>
+                </div>
+            </div>
+            <div class="bg-white rounded-xl px-5 py-3 flex items-center gap-5">
+                <div class="legend-icon rounded-full bg-[#00296B] w-3 h-3"></div>
+                <div>
+                    <h1 class="font-bold font-quicksand text-lg mb-1">LGU Pili Tech4ED Center Launching</h1>
+                    <p class="font-quicksand text-sm">March 28, 2023</p>
+                    <p class="mt-0 font-quicksand text-sm">08:00AM - 5:00AM</p>
+                </div>
+            </div>
+            <div class="bg-white rounded-xl px-5 py-3 flex items-center gap-5">
+                <div class="legend-icon rounded-full bg-[#C1121F] w-3 h-3"></div>
+                <div>
+                    <h1 class="font-bold font-quicksand text-lg mb-1">LGU Pili Tech4ED Center Launching</h1>
+                    <p class="font-quicksand text-sm">March 28, 2023</p>
+                    <p class="mt-0 font-quicksand text-sm">08:00AM - 5:00AM</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
