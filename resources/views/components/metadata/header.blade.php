2e720a4a72b00182410251feeb5e3d4b6801f21c
iAmWayneBenedict <iraolawayne@gmail.com>
2023-04-11 10:53:57 +0800

    resolve conflict banner

--- resources/views/components/metadata/header.blade.php
+++ resources/views/components/metadata/header.blade.php
@@ -11,6 +11,7 @@
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=inter:100,200,300,400,500,600,700,800,900" rel="stylesheet" />
 
+<wireui:scripts />
 <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
 
