0b90777acbecde16a1801dd583f611a65940d176
RlphSpncr <raptalagtag@gmail.com>
2023-04-12 11:02:35 +0800

    Fixed issue on web.php file, and banner.blade.php  : merge conflict

--- resources/views/components/layouts/banner.blade.php
+++ resources/views/components/layouts/banner.blade.php
@@ -1,10 +1,10 @@
 <style>
-    {
-  background: rgb(2,0,36);
-  background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%);
+    #banner-con {
+        background: rgb(2,0,36);
+        background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%);
     }
   </style>
-
+  
 <div id="banner-con" class="banner-con w-full h-[5.5rem] md:h-[7rem] flex justify-center items-center">
     <div class="mx-10 w-full flex items-center justify-between">
         <div class="left">
