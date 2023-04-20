17309e65ed1b3665da9c0a6f1d89b6c8a570fb3a
Christian Quelonio <chrquelonio@gmail.com>
2023-04-20 09:50:48 +0800

    fixed the user model

--- app/Models/Examinee/Users.php
+++ app/Models/Examinee/Users.php
@@ -9,28 +9,28 @@ class Users extends Model
 {
     use HasFactory;
 
-    protected $table = 'users';
+    protected $table = 'user_login';
     protected $fillable = [
         'google_id',
         'email',
         'fname',
         'lname',
-        'mname',
-        'password',
-        'place_of_birth',
-        'gender',
-        'citizenship',
-        'civil_status',
-        'contact_number',
-        'present_office',
-        'designation',
-        'telephone_number',
-        'office_address',
-        'office_category',
-        'no_of_years_in_pos',
-        'programming_langs',
-        'e_sign',
-        'date_accomplished',
+        // 'mname',
+        // 'password',
+        // 'place_of_birth',
+        // 'gender',
+        // 'citizenship',
+        // 'civil_status',
+        // 'contact_number',
+        // 'present_office',
+        // 'designation',
+        // 'telephone_number',
+        // 'office_address',
+        // 'office_category',
+        // 'no_of_years_in_pos',
+        // 'programming_langs',
+        // 'e_sign',
+        // 'date_accomplished',
     ];
 
     public $timestamps = false;
