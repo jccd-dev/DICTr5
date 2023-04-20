258f6dabfbb9011068b3b5b17af8459b1b9b3ce8
Christian Quelonio <chrquelonio@gmail.com>
2023-04-20 09:43:54 +0800

    Initial Email Notif

--- app/Http/Controllers/Examinee/DashboardController.php
+++ app/Http/Controllers/Examinee/DashboardController.php
@@ -3,13 +3,21 @@
 namespace App\Http\Controllers\Examinee;
 
 use App\Http\Controllers\Controller;
+use App\Mail\EmailUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Mail;
 
 class DashboardController extends Controller
 {
     public function dashboard(){
         dd(session()->get('user'));
         return view('layouts.examinee.dashboard');
+    }
+
+    public function sendEmail(){
+        // //
+        // Mail::to('@gmail.com')->send(new EmailUsers());
+        return view('layouts.email.passed');
     }
 }
