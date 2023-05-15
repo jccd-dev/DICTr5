b164be1129fceff71f3aa38c2110325ee3316348
iAmWayneBenedict <iraolawayne@gmail.com>
2023-05-15 13:59:35 +0800

    added examinees list page

---
+++ resources/js/utils/formdata.js
@@ -0,0 +1,10 @@
+const formdata = (el) => {
+    const formData = {};
+    const elements = el.querySelectorAll("select, input, textarea");
+
+    elements.forEach((e) => {
+        formData[e.name] = e.value;
+    });
+
+    return formData;
+};
--- routes/web.php
+++ routes/web.php
@@ -41,7 +41,7 @@ Route::get('/testing', \App\Http\Livewire\CMS\Testing::class);
 Route::get('/about', function () {
     return view('pages.about');
 });
-Route::get('/announcement/{id}',[ViewAnnouncementController::class, 'view_announcement'])->name('view.announcement');
+Route::get('/announcement/{id}', [ViewAnnouncementController::class, 'view_announcement'])->name('view.announcement');
 
 // ADMIN SIDE ROUTES
 Route::prefix('admin')->group(function () {
@@ -49,7 +49,7 @@ Route::prefix('admin')->group(function () {
     Route::get('/login', Login::class)->name("admin.login")->middleware(['jwt.isLoggedIn']);
     Route::get('/logout', [AdminLoginController::class, 'logout'])->name("admin.logout");
 
-    Route::middleware(['jwt.logAuth'])->group(function (){
+    Route::middleware(['jwt.logAuth'])->group(function () {
         Route::get('/dashboard', Dashboard::class)->name("admin.dashboard");
         Route::get('/test', [AdminLoginController::class, 'test'])->name("admin.test");
 
@@ -63,7 +63,7 @@ Route::prefix('admin')->group(function () {
         });
 
         // manage admin accounts
-        Route::prefix('dict-admins')->group(function (){
+        Route::prefix('dict-admins')->group(function () {
             Route::get('/', 'AdminAccounts@render')->name('admin.accounts');
             Route::post('/create', [AdminAccounts::class, 'add_admin'])->name('admin.create');
             Route::get('/view/{id}', [AdminAccounts::class, 'access_admin'])->name('admin.access');
@@ -71,8 +71,8 @@ Route::prefix('admin')->group(function () {
             Route::delete('/delete/{id}', [AdminAccounts::class, 'delete_admin'])->name('admin.delete');
         });
 
-        Route::prefix('examinee')->group( function (){
-            Route::get('/', [Applicants::class, 'render'])->name('examinees');
+        Route::prefix('examinee')->group(function () {
+            Route::get('/', [Applicants::class, 'render'])->name('admin.examinees');
             Route::get('/search', [Applicants::class, 'search_examinees'])->name('search');
         });
 
