0d580aa974b824d6d93110d4c6f840e466506997
iAmWayneBenedict <iraolawayne@gmail.com>
2023-05-17 12:57:56 +0800

    added admin accounts, refactored add-applicants, added delete modal admin-accounts

--- app/Http/Controllers/Admins/AdminAccounts.php
+++ app/Http/Controllers/Admins/AdminAccounts.php
@@ -22,15 +22,17 @@ class AdminAccounts extends Controller
     ];
 
     private $adminModel;
-    public function __construct(){
+    public function __construct()
+    {
         $this->adminModel = new AdminModel;
     }
 
     //TODO: view for adding new admins
-    public function render(): View{
+    public function render(): View
+    {
 
         $admins = AdminModel::all();
-        return view('', ['admins' => $admins]);
+        return view('AdminFunctions.admin-accounts', ['data' => $admins]);
     }
 
     /**
@@ -38,11 +40,12 @@ class AdminAccounts extends Controller
      * @return
      * @description Function for creating new admin
      */
-    public function add_admin(Request $request): JsonResponse{
+    public function add_admin(Request $request): JsonResponse
+    {
 
-        $validator = Validator::make($request->all(),$this->rules);
+        $validator = Validator::make($request->all(), $this->rules);
 
-        if(!$validator->fails()){
+        if (!$validator->fails()) {
             return response()->json(['errors' => $validator->errors()], 422);
         }
 
@@ -56,7 +59,7 @@ class AdminAccounts extends Controller
             'designation' => $request->designation,
         ]);
 
-        if(!$new_admin->save()){
+        if (!$new_admin->save()) {
             // return server error if data not isnerted to database
             return response()->json(['error' => 'Server Error'], 500);
         }
@@ -70,7 +73,6 @@ class AdminAccounts extends Controller
                 'name' => $new_admin->name
             ]
         ], 200);
-
     }
 
     /**
@@ -86,9 +88,9 @@ class AdminAccounts extends Controller
     public function update_admin(Request $request, int $admin_id): JsonResponse
     {
 
-        $validator = Validator::make($request->all(),$this->rules);
+        $validator = Validator::make($request->all(), $this->rules);
 
-        if(!$validator->fails()){
+        if (!$validator->fails()) {
             return response()->json(['errors' => $validator->errors()], 422);
         }
 
@@ -111,18 +113,18 @@ class AdminAccounts extends Controller
         $admin_to_update->role = $request->role;
         $admin_to_update->designation = $request->designation;
 
-        if(!$admin_to_update->save()){
+        if (!$admin_to_update->save()) {
             return response()->json(['error' => 'Server Error'], 500);
         }
 
         return response()->json(['admn' => $admin_to_update->name], 200);
     }
 
-    public function delete_admin(int $admin_id): bool{
+    public function delete_admin(int $admin_id): bool
+    {
 
         $admin_to_delete = AdminModel::find($admin_id);
 
         return $admin_to_delete->delete() > 0;
     }
-
 }
--- resources/views/AdminFunctions/result.blade.php
+++ resources/views/AdminFunctions/result.blade.php
@@ -1,10 +1,102 @@
-@foreach ($data as $user)
-    <tr>
-        <td>{{ $user->formatted_name }}</td>
-        <td>{{ $user->gender }}</td>
-        <td>{{ $user->current_status }}</td>
-        <td>{{ $user->tertiaryEdu ? $user->tertiaryEdu->degree : '' }}</td>
-        <td>{{ $user->addresses ? $user->addresses->formatted_address : '' }}</td>
-    </tr>
+@foreach ($data as $key => $user)
+    @if($key % 2 === 0)
+        <tr class="bg-[#FDC500] bg-opacity-25">
+            <th scope="row" class="px-6 py-4 font-medium whitespace-nowrap">
+                {{ $user->formatted_name }}
+            </th>
+            <td class="px-6 py-4">
+                {{ ucfirst($user->gender) }}
+            </td>
+            <td class="px-6 py-4">
+                {{ ucfirst($user->current_status) }}
+            </td>
+            <td class="px-6 py-4">
+                {{ $user->addresses ? $user->addresses->formatted_address : '' }}
+            </td>
+            <td class="px-6 py-4">
+                {{ $user->tertiaryEdu ? $user->tertiaryEdu->degree : '' }}
+            </td>
+            <td class="px-6 py-4">
+                <select class="bg-[#00509D] bg-opacity-10 border-none text-black text-sm rounded-2xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <option selected>Choose status</option>
+                    <option value="0">Disapproved</option>
+                    <option value="1">Incomplete Requirements</option>
+                    <option value="2">For Evaluation</option>
+                    <option value="3">Approved</option>
+                    <option value="4">Waiting for Result</option>
+                </select>
+            </td>
+            <td class="px-6 py-4">
+                <select class="bg-[#00509D] bg-opacity-10 border-none text-black text-sm rounded-2xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <option selected>Choose status</option>
+                    <option value="0">Failed</option>
+                    <option value="1">Processing</option>
+                    <option value="2">Passed</option>
+                </select>
+            </td>
+            <td class="px-6 py-4">
+                <button
+                    data-popover-target="popover-animation"
+                    data-popover-trigger="click"
+                    data-popover-placement="bottom"
+                    type="button"
+                    class="bg-[#00509D] bg-opacity-10 rounded-xl p-2"
+                    >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="w-4 h-4">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                    </svg>
+                </button>
+            </td>
+        </tr>
+    @else
+        <tr class="bg-[#FDC500] bg-opacity-10">
+            <th scope="row" class="px-6 py-4 font-medium whitespace-nowrap">
+                {{ $user->formatted_name }}
+            </th>
+            <td class="px-6 py-4">
+                {{ $user->gender }}
+            </td>
+            <td class="px-6 py-4">
+                {{ $user->current_status }}
+            </td>
+            <td class="px-6 py-4">
+                {{ $user->addresses ? $user->addresses->formatted_address : '' }}
+            </td>
+            <td class="px-6 py-4">
+                {{ $user->tertiaryEdu ? $user->tertiaryEdu->degree : '' }}
+            </td>
+            <td class="px-6 py-4">
+                <select class="bg-[#00509D] bg-opacity-10 border-none text-black text-sm rounded-2xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <option selected>Choose status</option>
+                    <option value="0">Disapproved</option>
+                    <option value="1">Incomplete Requirements</option>
+                    <option value="2">For Evaluation</option>
+                    <option value="3">Approved</option>
+                    <option value="4">Waiting for Result</option>
+                </select>
+            </td>
+            <td class="px-6 py-4">
+                <select class="bg-[#00509D] bg-opacity-10 border-none text-black text-sm rounded-2xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <option selected>Choose status</option>
+                    <option value="0">Failed</option>
+                    <option value="1">Processing</option>
+                    <option value="2">Passed</option>
+                </select>
+            </td>
+            <td class="px-6 py-4">
+                <button
+                    data-popover-target="popover-animation"
+                    data-popover-trigger="click"
+                    data-popover-placement="bottom"
+                    type="button"
+                    class="bg-[#00509D] bg-opacity-10 rounded-xl p-2"
+                    >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="w-4 h-4">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                    </svg>
+                </button>
+            </td>
+        </tr>
+    @endif
 @endforeach
 {{$data->links()}}
