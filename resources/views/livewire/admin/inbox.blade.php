c1de6c3d8f9b4a04490d921e60ca25e01cb4bb6f
Christian Quelonio <chrquelonio@gmail.com>
2023-05-24 16:56:49 +0800

    fix the ui in inbox

--- resources/views/livewire/admin/inbox.blade.php
+++ resources/views/livewire/admin/inbox.blade.php
@@ -64,7 +64,7 @@
                                             <span class="text-xs {{(!$fb->is_read) ? 'font-semibold' : '' }}">{{date('m/d/Y h:i A', strtotime($fb->timestamp))}}</span>
                                         </p>
                                    </div>
-                                    <div wire:click="prepare_to_delete({{$fb->id}})" class="basis-1/12 flex justify-end" data-modal-target="feedback_delete_modal" data-modal-toggle="feedback_delete_modal">
+                                    <div wire:click="prepare_to_delete({{$fb->id}})" class="basis-1/12 flex justify-end">
                                         <svg fill="none" class="w-5 text-right cursor-pointer text-red-700" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"></path>
                                         </svg>
@@ -96,28 +96,68 @@
                     </div>
                 </div>
             @elseif($content == 2)
-                  <div class="flex flex-row gap-3">
-                      <div class="basis-2/6">
-                          @forelse ($tech4ed_requests as $tech4ed)
-                              <div class="border border-2 {{(!$tech4ed->is_read) ? 'bg-yellow-300' : 'bg-yellow-100 border' }} py-2 px-4 cursor-pointer rounded mb-2 ">
-                                  <p class="leading-3">
-                                      <span class="text-lg font-bold">{{$tech4ed->name}}</span>
-                                      <br>
-                                      <span class="text-sm font-semibold">{{$tech4ed->email}} | {{$tech4ed->cp_number}}</span>
-                                      <br>
-                                      <span class="text-xs font-semibold">{{date('m/d/Y h:i A', strtotime($tech4ed->timestamp))}}</span>
-                                  </p>
-                              </div>
-                          @empty
-                              <div>
-                                  No Found Feedback
-                              </div>
-                          @endforelse
-                          {{ $tech4ed_requests->links() }}
-                      </div>
-                      <div class="basis-4/6">
 
-                      </div>
+
+                  <div class="relative overflow-x-auto">
+                      <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                          <tr>
+                              <th scope="col" class="px-6 py-3">
+                                  Name
+                              </th>
+                              <th scope="col" class="px-6 py-3">
+                                  Email
+                              </th>
+                              <th scope="col" class="px-6 py-3">
+                                  Cellphone Number
+                              </th>
+                              <th scope="col" class="px-6 py-3">
+                                  Organization
+                              </th>
+                              <th scope="col" class="px-6 py-3">
+                                  Tech4Ed Course
+                              </th>
+                              <th scope="col" class="px-6 py-3">
+                                  Action
+                              </th>
+                          </tr>
+                          </thead>
+                          <tbody>
+                          @forelse ($tech4ed_requests as $tech4ed)
+                              <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                  <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                      {{$tech4ed->name}}
+                                  </th>
+                                  <td class="px-6 py-4">
+                                      {{$tech4ed->email}}
+                                  </td>
+                                  <td class="px-6 py-4">
+                                      {{$tech4ed->cp_number}}
+                                  </td>
+                                  <td class="px-6 py-4">
+                                      {{$tech4ed->organization}}
+                                  </td>
+                                  <td class="px-6 py-4">
+                                      {{$tech4ed->tech4ed_course_training}}
+                                  </td>
+                                  <td class="px-6 py-4">
+                                      <div wire:click="prepare_to_delete({{$tech4ed->id}})">
+                                          <svg fill="none" class="w-5 text-right cursor-pointer text-red-700" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                              <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"></path>
+                                          </svg>
+                                      </div>
+                                  </td>
+                              </tr>
+                          @empty
+                              <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                  <th colspan="6" scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                      No Available Data
+                                  </th>
+                              </tr>
+                          @endforelse
+                          </tbody>
+                          {{ $tech4ed_requests->links() }}
+                      </table>
                   </div>
             @else
                 This is
@@ -126,22 +166,18 @@
     </div>
 
     {{-- Delete Modal for Feedback --}}
-    <div wire:ignore id="feedback_delete_modal" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
-        <div class="relative w-full max-w-md max-h-full">
-            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="feedback_delete_modal">
-                    <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                    <span class="sr-only">Close modal</span>
-                </button>
-                <div class="p-6 text-center">
-                    <svg aria-hidden="true" class="mx-auto mb-4 text-gray-400 w-14 h-14 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                    <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this feedback?</h3>
-                    <button wire:click="delete_feedback" data-modal-hide="feedback_delete_modal" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
-                        Yes, I'm sure
-                    </button>
-                    <button data-modal-hide="feedback_delete_modal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No, cancel</button>
+    <x-modal wire:model.defer="feedback_modal" blur="true" align="center" max-width="sm">
+        <x-card title="Delete Confirmation">
+            <p class="text-gray-600">
+                Are you sure you want to delete the feedback?
+            </p>
+
+            <x-slot name="footer">
+                <div class="flex justify-end gap-x-4">
+                    <x-button secondary label="Cancel" x-on:click="close" />
+                    <x-button wire:click="delete_feedback" red label="Yes, delete it!" />
                 </div>
-            </div>
-        </div>
-    </div>
+            </x-slot>
+        </x-card>
+    </x-modal>
 </div>
