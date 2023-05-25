59fb40ce34a1d96c7948ccf65dbc244e53938c19
Christian Quelonio <chrquelonio@gmail.com>
2023-05-25 16:45:04 +0800

    fix ui of admin exam schedule

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -23,6 +23,7 @@
                             @elseif(request()->routeIs('admin.examinees')) {{'Exam Applicants'}}
                             @elseif(request()->routeIs('admin.accounts')) {{'Admin Accounts'}}
                             @elseif(request()->routeIs('admin.inbox')) {{'Inbox'}}
+                            @elseif(request()->routeIs('admin.exam-schedule')) {{'Exam Schedule'}}
                             @else {{'DICT Camarines Sur'}}
                             @endif
                         </h2>
