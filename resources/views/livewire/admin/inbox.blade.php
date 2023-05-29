a392000ab0386bd65c791a9d4cfe42e650405e26
Christian Quelonio <chrquelonio@gmail.com>
2023-05-29 13:54:58 +0800

    inbox ui

--- resources/views/livewire/admin/inbox.blade.php
+++ resources/views/livewire/admin/inbox.blade.php
@@ -71,7 +71,7 @@
                                     </div>
                                 </div>
                             @empty
-                                <div>
+                                <div class="p-10">
                                     No Found Feedback
                                 </div>
                             @endforelse
@@ -169,7 +169,10 @@
                                   Email
                               </th>
                               <th scope="col" class="px-6 py-3">
-                                  Sent Email
+                                  Date
+                              </th>
+                              <th scope="col" class="px-6 py-3">
+                                  Intended For
                               </th>
                               <th scope="col" class="px-6 py-3">
                                   Action
@@ -177,33 +180,37 @@
                           </tr>
                           </thead>
                           <tbody>
+                          @forelse($sent_emails as $sent_email)
                               <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                                   <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                      Christian Quelonio
+                                      {{$sent_email->user}}
                                   </th>
                                   <td class="px-6 py-4">
-                                      chrquelonio@gmail.com
+                                      {{$sent_email->email}}
                                   </td>
                                   <td class="px-6 py-4">
-                                      Admin Account
+                                      {{date('M d, Y | h:i A', strtotime($sent_email->timestamp))}}
                                   </td>
                                   <td class="px-6 py-4">
-                                      <div wire:click="prepare_to_delete_sent_email(1)">
+                                      {{$sent_email->intended_for}}
+                                  </td>
+                                  <td class="px-6 py-4">
+                                      <div wire:click="prepare_to_delete_sent_email({{$sent_email->id}})">
                                           <svg fill="none" class="w-5 text-right cursor-pointer text-red-700" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                               <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"></path>
                                           </svg>
                                       </div>
                                   </td>
                               </tr>
-{{--                          @empty--}}
-{{--                              <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">--}}
-{{--                                  <th colspan="6" scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">--}}
-{{--                                      No Available Data--}}
-{{--                                  </th>--}}
-{{--                              </tr>--}}
-{{--                          @endforelse--}}
+                          @empty
+                              <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                  <th colspan="6" scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                      No Available Data
+                                  </th>
+                              </tr>
+                          @endforelse
                           </tbody>
-{{--                          {{ $tech4ed_requests->links() }}--}}
+                          {{ $sent_emails->links() }}
                       </table>
                   </div>
             @endif
