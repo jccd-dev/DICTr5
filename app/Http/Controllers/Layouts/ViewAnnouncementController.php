6aa7d0e08c91ff13896c0162c0f647703e0098d1
konnichiwaJCCD <jccd0724@gmail.com>
2023-05-31 14:25:11 +0800

    for temporary hosting

--- app/Http/Controllers/Layouts/ViewAnnouncementController.php
+++ app/Http/Controllers/Layouts/ViewAnnouncementController.php
@@ -2,16 +2,35 @@
 
 namespace App\Http\Controllers\Layouts;
 
-use App\Http\Controllers\Controller;
-use App\Models\CMS\Announcement;
 use Illuminate\Http\Request;
+use App\Models\CMS\Announcement;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 
 class ViewAnnouncementController extends Controller
 {
+
+    public $usersCounter = [];
+    public function mount(){
+
+        if (!session('visited')) {
+            session(['visited' => true]);
+
+            DB::table('visitor_count')->increment('visitors');
+        }
+
+        $this->usersCounter['visitor'] = DB::table('visitor_count')->value('visitors');
+        $this->usersCounter['applicants'] = DB::table('visitor_count')->value('applicants');
+        $this->usersCounter['passers'] = DB::table('visitor_count')->value('passers');
+
+    }
     //
     public function view_announcement(string $id){
+        $this->mount();
+        $this->usersCounter['registered'] = DB::table('users_data')->count();
         return view('pages.announcement', [
-            'announcement' => Announcement::where('id', $id)->first()
+            'announcement' => Announcement::where('id', $id)->first(),
+            'visitors'     => $this->usersCounter
         ]);
     }
 }
