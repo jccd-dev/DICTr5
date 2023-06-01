ca1564e191d01e717556c227d5f9673395668545
konnichiwaJCCD <jccd0724@gmail.com>
2023-06-01 10:43:37 +0800

    add and fixed the sending of email for exam results

--- resources/views/AdminFunctions/applicant-data.blade.php
+++ resources/views/AdminFunctions/applicant-data.blade.php
@@ -290,7 +290,7 @@
             @csrf
             <div class="p-4 overflow-y-auto">
                 <label for="exam-result" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
-                <select id="exam-result" x-model="result" name="exam-result" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <select id="exam-result" x-model="result" name="exam_result" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                     <option value="passed" selected>Passed</option>
                     <option value="failed">Failed</option>
                 </select>
